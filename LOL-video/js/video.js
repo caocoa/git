306b880ffbd839ec8d0cd88ed09a1c265dbc6f7b
qwe <laoli@qq.com>
2017-11-06 13:56:12 +0800

    20171106

--- LOL-video/js/video.js
+++
@@ -1,9 +0,0 @@
-var oaltsp=document.querySelector(".alt-sp");
-var oaltmp4=document.getElementById("alt_MP4");
-
-oaltsp.onclick = function(){
-	oaltmp4.style.display = "block";
-}
-oaltmp4.children[0].children[1].onclick = function(){
-	oaltmp4.style.display = "none";
-}
